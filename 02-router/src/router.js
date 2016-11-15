e0fdd47b5f22f29a30f38530689d7d9f8657d108
rudolfsonjunior <[omitted]>
2016-11-15 19:43:39 +0100

    CHANGED router whitespace

--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,6 +1,6 @@
 const routes = new Map();
 
-function BackButtonListener() {
+function backButtonListener() {
 
   window.onpopstate = (e) => {
 
@@ -10,20 +10,26 @@ function BackButtonListener() {
 
 }
 
-function ClickListener() {
+function clickListener() {
 
   const links = document.getElementsByTagName('a');
 
   for (let i = 0; i < links.length; i++) {
+    
     links[i].addEventListener('click', e => {
+      
       e.preventDefault();
-      console.log(e.srcElement);
+      
       try {
+        
         if (e.srcElement.attributes.rel.value == 'external') return openNewTab(e.srcElement.href);
         else if (e.srcElement.attributes.rel.value == 'download') return console.log('download');
+        
       } catch (err) {
+        
         history.pushState({path: e.srcElement.pathname}, '', e.srcElement.href);
         goto(e.srcElement.pathname);
+        
       }
     });
 
@@ -54,20 +60,25 @@ function goto(path) {
     }
 
     if (splittedRoute[1] == splittedPath[1]) {
+      
       let count = splittedPath.length;
       let params = splittedPath.splice(2, count);
       return fn(...params);
+      
     }
 
   }
   if (routes.has('*')) {
+    
     return routes.get('*')();
 
   }
 }
 
 function openNewTab(url) {
+  
   window.open(url, '_blank');
+  
 }
 
 export default function (route, fn) {
@@ -81,8 +92,8 @@ export default function (route, fn) {
   if (!route && !fn) {
 
     init();
-    ClickListener();
-    BackButtonListener();
+    clickListener();
+    backButtonListener();
 
   }
 }
